c91a01398172e815c460bbb2af0d4fa6c7f6dff4
Jason Garber <jason@sixtwothree.org>
2024-02-06 10:23:18 -0500

    chore: Remove test-related dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
-const ava = require('@jgarber/eslint-config/ava');
 const config = require('@jgarber/eslint-config');
 
 module.exports = [
   {
     ignores: ['coverage']
   },
-  ...config,
-  ...ava
+  ...config
 ];
